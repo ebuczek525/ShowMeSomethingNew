2ce9245b49c57e4db6910e289eca594e9a617681
ebuczek525 <45291712+ebuczek525@users.noreply.github.com>
2019-05-01 18:47:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,133 @@
-const app = document.getElementById('root')
+function getSearch() {
 
-const container = document.createElement('div')
-container.setAttribute('class', 'container')
+    var input = document.getElementById('valueID').value
 
-app.appendChild(container)
+    const app = document.getElementById('root')
 
-var request = new XMLHttpRequest()
-var query = prompt("Movie/series name?")
+    const container = document.createElement('div')
+    container.setAttribute('class', 'container')
 
-request.open('GET', 'https://api.themoviedb.org/3/search/multi?api_key=bc61dce55242b247bc2d74102f24c074&language=en-US&query=' + query + '&page=1&include_adult=false', true)
-request.onload = function() {
+    app.appendChild(container)
 
-  var data = JSON.parse(this.response)
-  if (request.status >= 200 && request.status < 400) {
-    data.results.forEach(movie => {
-      const card = document.createElement('div')
-      card.setAttribute('class', 'card')
+    var request = new XMLHttpRequest()
 
-      const h1 = document.createElement('h1')
-      h1.innerHTML = movie.title + movie.name
+    request.open('GET', 'https://api.themoviedb.org/3/search/multi?api_key=bc61dce55242b247bc2d74102f24c074&language=en-US&query=' + input + '&page=1&include_adult=false', true)
 
-      const p = document.createElement('p')
-      p.innerHTML = 'Release date: ' + `${movie.release_date}` + `${movie.first_air_date}` + '<br>' + 'Type: ' + `${movie.media_type} ` + '<br>' + 'TMDb Score: ' + `${movie.vote_average}`
+    request.onload = function() {
 
-      const i = document.createElement('img')
-      i.setAttribute('src', 'http://image.tmdb.org/t/p/w342/' + `${movie.poster_path}`)
+        var data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            data.results.forEach(movie => {
+                const card = document.createElement('div')
+                card.setAttribute('class', 'card')
 
-      const h4 = document.createElement('h4')
-      h4.innerHTML = movie.overview
+                const h1 = document.createElement('h1')
+                if (`${movie.media_type}` == 'tv') {
+                    h1.innerHTML = movie.name
+                } else {
+                    h1.innerHTML = movie.title
+                }
 
-      const a = document.createElement('a')
-      a.setAttribute('href', 'https://www.themoviedb.org/' + `${movie.media_type}` + '/' + `${movie.id}`)
-      a.innerHTML = 'TMDb Link'
+                const p = document.createElement('p')
+                if (`${movie.media_type}` == 'tv') {
+                    p.innerHTML = 'First air date: ' + `${movie.first_air_date}` + '<br>' + 'TMDb Score: ' + `${movie.vote_average}`
+                } else {
+                    p.innerHTML = 'Release date: ' + `${movie.release_date}` + '<br>' + 'TMDb Score: ' + `${movie.vote_average}`
+                }
 
-      container.appendChild(card)
-      card.appendChild(h1)
-      card.appendChild(p)
-      card.appendChild(i)
-      card.appendChild(h4)
-      card.appendChild(a)
+                const i = document.createElement('img')
+                i.setAttribute('src', 'http://image.tmdb.org/t/p/w342/' + `${movie.poster_path}`)
 
-    })
-  } else {
-    const errorMessage = document.createElement('marquee')
-    errorMessage.textContent = `Encountered an error!!`
-    app.appendChild(errorMessage)
-  }
+                const h4 = document.createElement('h4')
+                h4.innerHTML = movie.overview
+
+                const a = document.createElement('a')
+                a.setAttribute('href', 'https://www.themoviedb.org/' + `${movie.media_type}` + '/' + `${movie.id}`)
+                a.innerHTML = 'TMDb Link'
+
+                container.appendChild(card)
+                card.appendChild(h1)
+                card.appendChild(p)
+                card.appendChild(i)
+                card.appendChild(h4)
+                card.appendChild(a)
+
+            })
+        } else {
+            const errorMessage = document.createElement('marquee')
+            errorMessage.textContent = `Encountered an error!!`
+            app.appendChild(errorMessage)
+        }
+    }
+
+    request.send();
+
 }
 
-request.send();
+function getResults() {
+
+    var c_type = document.getElementById('type').value
+    var c_genre = document.getElementById('genre').value
+    var c_score = document.getElementById('score').value
+
+    const app = document.getElementById('root')
+
+    const container = document.createElement('div')
+    container.setAttribute('class', 'container')
+
+    app.appendChild(container)
+
+    var request = new XMLHttpRequest()
+
+    request.open('GET', 'https://api.themoviedb.org/3/discover/' + c_type + '?api_key=bc61dce55242b247bc2d74102f24c074&language=en-US&include_adult=false&include_video=false&page=1&vote_average.gte=' + c_score + '&with_genres=' + c_genre, true)
+
+    request.onload = function() {
+
+        var data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            data.results.forEach(movie => {
+                const card = document.createElement('div')
+                card.setAttribute('class', 'card')
+
+                const h1 = document.createElement('h1')
+                if (c_type == 'tv') {
+                    h1.innerHTML = movie.name
+                } else {
+                    h1.innerHTML = movie.title
+                }
+
+                const p = document.createElement('p')
+                if (c_type == 'tv') {
+                    p.innerHTML = 'First air date: ' + `${movie.first_air_date}` + '<br>' + 'TMDb Score: ' + `${movie.vote_average}`
+                } else {
+                    p.innerHTML = 'Release date: ' + `${movie.release_date}` + '<br>' + 'TMDb Score: ' + `${movie.vote_average}`
+                }
+
+                const i = document.createElement('img')
+                i.setAttribute('src', 'http://image.tmdb.org/t/p/w342/' + `${movie.poster_path}`)
+
+                const h4 = document.createElement('h4')
+                h4.innerHTML = movie.overview
+
+                const a = document.createElement('a')
+                a.setAttribute('href', 'https://www.themoviedb.org/' + c_type + '/' + `${movie.id}`)
+                a.innerHTML = 'TMDb Link'
+
+                container.appendChild(card)
+                card.appendChild(h1)
+                card.appendChild(p)
+                card.appendChild(i)
+                card.appendChild(h4)
+                card.appendChild(a)
+
+            })
+        } else {
+            const errorMessage = document.createElement('marquee')
+            errorMessage.textContent = `Encountered an error!!`
+            app.appendChild(errorMessage)
+        }
+    }
+
+    request.send();
+
+}
